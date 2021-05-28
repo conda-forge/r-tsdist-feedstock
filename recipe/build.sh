30d1401607746e9b4b45101b2e35c35e293cce1c
Devon Ryan <dpryan79@users.noreply.github.com>
2021-05-28 16:26:04 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ ${HOST} =~ .*linux.* ]]; then
-    export LD_LIBRARY_PATH=${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64
+    export LD_LIBRARY_PATH=${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/lib64:${BUILD_PREFIX}/lib
 fi
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
